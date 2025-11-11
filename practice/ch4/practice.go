9b5137144f3b00f5795318a4c1b4364d303984e5
Keita Tanemori <ku74n1@gmail.com>
2025-11-12 08:45:53 +0900

    if文・switch文の学習
    - if文を書くときJSでは書く必要がる{}はいらない
    - iotaを使うとブロックの先頭で０から始まり次の定数を宣言する度（行ごと）に１ずつ自動で増加する
    - switch文ではそれぞれのケース文でbrackを書く必要がない
    - ケース分にfallthroughを書くと無条件で次のケース文を実行する

--- practice/ch4/practice.go
+++ practice/ch4/practice.go
@@ -4,42 +4,64 @@ import (
 	"fmt"
 )
 func main() {
-	var r float64
-	fmt.Println("数字を入力してください")
-	fmt.Scan(&r)
-	const Pi = 3.14159265
-	area := Pi * r * r
-	fmt.Println(fmt.Sprintf("%.2f", area))
+	// var r float64
+	// fmt.Println("数字を入力してください")
+	// fmt.Scan(&r)
+	// const Pi = 3.14159265
+	// area := Pi * r * r
+	// fmt.Println(fmt.Sprintf("%.2f", area))
 
-	var d int
-	fmt.Println("数字を入力してください")
-	fmt.Scan(&d)
-	const (
-		Sun = iota
-		Mon
-		Tue
-		Wed
-		Thu
-		Fri
-		Sat
-	)
+	// var d int
+	// fmt.Println("数字を入力してください")
+	// fmt.Scan(&d)
+	// const (
+	// 	Sun = iota
+	// 	Mon
+	// 	Tue
+	// 	Wed
+	// 	Thu
+	// 	Fri
+	// 	Sat
+	// )
 
-	switch d {
-	case Sun:
-		fmt.Println("Sun")
-	case Mon:
-		fmt.Println("Mon")
-	case Tue:
-		fmt.Println("Tue")
-	case Wed:
-		fmt.Println("Wed")
-	case Thu:
-		fmt.Println("Thr")
-	case Fri:
-		fmt.Println("Fri")
-	case Sat:
-		fmt.Println("Sat")
-	default:
-		fmt.Println("invalid")
-	}
+	// switch d {
+	// case Sun:
+	// 	fmt.Println("Sun")
+	// case Mon:
+	// 	fmt.Println("Mon")
+	// case Tue:
+	// 	fmt.Println("Tue")
+	// case Wed:
+	// 	fmt.Println("Wed")
+	// case Thu:
+	// 	fmt.Println("Thr")
+	// case Fri:
+	// 	fmt.Println("Fri")
+	// case Sat:
+	// 	fmt.Println("Sat")
+	// default:
+	// 	fmt.Println("invalid")
+	// }
+
+	// var n int = 10
+	// fmt.Scanf("%d", &n)
+	// if n%2 == 0 {
+	// 	fmt.Println("偶数です")
+	// } else {
+	// 	fmt.Println("奇数です")
+	// }
+
+	// var num int 
+	// fmt.Println("西暦を入力してください")
+	// fmt.Scanf("%d", &num)
+
+	// if num%400 == 0 {
+	// 	fmt.Println("Leap Year")
+	// }else if num % 100 == 0 {
+	// 	fmt.Println("Common Year")
+	// }else if num % 4 == 0 {
+	// 	fmt.Println("Leap Year")
+	// }else{
+	// 	fmt.Println("Common Year")
+	// }
 }
