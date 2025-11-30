27f741adfadcf9dcd45cbd489fa2152a20216fab
Keita Tanemori <ku74n1@gmail.com>
2025-11-30 16:48:42 +0900

    deferについて - LIFO(後入れ先出し)である - 関数が終了した時の処理を書く - JSのtry/catch/finalyのfinalyにあたる処理

---
+++ practice/ch5/defer.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	deferExample()
+	main2()
+	
+}
+
+func deferExample() int{
+	a := 10
+	defer func(val int){
+		fmt.Println("first:", val)
+	} (a)
+	a = 20
+	defer func (val int){
+		fmt.Println("second:", val)
+	}(a)
+	a =30
+	fmt.Println("exiting:",a)
+	return a
+}
+
+
